00385d1206b55e58fa55c675d67e5fe1537a29ba
NicolasOsmundo <nicolasosmundo@hotmail.com>
2021-03-26 12:57:47 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class Comparandoswitch
+{
+    static void Main(string[] args)
+    {
+        int idade = 16;
+        switch (idade)
+        {
+            case 15:
+                Console.WriteLine("SUB -15");
+                break;
+            case 16:
+                Console.WriteLine("SUB-17");
+                break;
+            case 17:
+                Console.WriteLine("SUB-17");
+                break;
+            case 18:
+                Console.WriteLine("SUB-20");
+                break;
+            default:
+                Console.WriteLine("Categoria não definida");
+                break;
+
+        }
+        Console.Read();
+    }
+
+}    
+
+        
+    
